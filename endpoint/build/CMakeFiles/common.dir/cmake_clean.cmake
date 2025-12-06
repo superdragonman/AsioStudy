9c0dfb11f5598b466cac397eac8d4b3a4827ed30
LiuZhaoyang <1343183241@qq.com>
2025-12-07 00:21:38 +0800

    endpoint

---
+++ endpoint/build/CMakeFiles/common.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/liuzhaoyang/C++Projects/AsioStudy/endpoint/bin/libcommon.dylib"
+  "/Users/liuzhaoyang/C++Projects/AsioStudy/endpoint/bin/libcommon.pdb"
+  "CMakeFiles/common.dir/src/endpoint.cpp.o"
+  "CMakeFiles/common.dir/src/endpoint.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ endpoint/main.cpp
@@ -0,0 +1,10 @@
+// Include a header from the Boost.Lambda library
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <endpoint.h>
+
+int main()
+{
+  std::cout << "Boost example: ";
+  return 0;
+}
